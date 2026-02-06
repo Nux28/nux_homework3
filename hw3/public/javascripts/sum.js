c33d4a15fdee5876a4415cada99774a7d12c6f13
Nux28 <nuxoll28@up.edu>
2026-02-05 22:56:36 -0800

    hw3

---
+++ hw3/public/javascripts/sum.js
@@ -0,0 +1,7 @@
+//josephine nuxoll
+//basic sum code for testing
+
+function sum(a, b) {
+return a + b;
+}
+module.exports = sum;
---
+++ hw3/public/tests/sum.test.js
@@ -0,0 +1,6 @@
+//basic unit test for sum code
+
+const sum = require('../public/javascripts/sum.js');
+	test('adds 1 + 2 to equal 3', () => {
+		expect(sum(1, 2)).toBe(3);
+});
